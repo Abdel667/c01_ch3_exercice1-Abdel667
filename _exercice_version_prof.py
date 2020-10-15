db96d1fb2a4a885537676a615be2c02abbe3d1be
Abdel667 <71038391+Abdel667@users.noreply.github.com>
2020-10-14 22:52:52 -0400

    corrigé

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -2,33 +2,48 @@
 
 
 def dissipated_power(voltage, resistance):
+	Puissance_dissipe= (voltage**2)/resistance
+	print('La puissance dissipée est de ')
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	return Puissance_dissipe
 
 def orthogonal(v1, v2):
+	produit_scalaire = v1[0]*v2[0]+ v1[1]*v2[1]
+	print('les vecteurs sont ils orthoghonaux?')
+	if (produit_scalaire == 0):
+		return True
+	else:
+		return False
+	
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	 # Pour accéder au X
+	 # Pour accéder au Y
+	 
 
 def average(values):
-	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	
+	somme=0# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		somme += v
+	moyenne= somme/len(values)	
+	print('la moyenne est')
+	return moyenne # La variable v contient une valeur de la liste.
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
-	while value != 0:
+	if value != 0:
 		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
-
-	return (twenties, tens, fives, twos, ones);
+			twenties= value // 20
+			value= value - twenties*20
+		if value >= 10:
+			tens= value // 10
+			value= value - tens*10
+		if value >= 5:
+			fives= value// 5
+			value= value - fives*5
+		if value >= 1:
+			ones= value
+	return (twenties, 'twenties', tens, 'tens', fives,'fives', ones, 'ones')
 
 if __name__ == "__main__":
 	print(dissipated_power(69, 420))
